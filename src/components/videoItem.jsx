75fe26270904e9fcb0d2cacd677f440cba47dadc
WebDevelop2 <dslgpgh@gmail.com>
2021-11-08 00:24:15 +0900

    add the thumbnail

--- src/components/videoItem.jsx
+++ src/components/videoItem.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = props => (
+
+
+
+
+<div className="item">
+<img src={props.video.snippet.thumbnails.default.url} alt="" />  {/* img 태그 디스플레이 블럭을 none 으로 바꿔주면 될 듯*/}
+<div>{props.video.snippet.title}</div>
+<div>{props.video.snippet.channelTitle}</div>
+</div>
+
+
+
+)
 
 export default VideoItem;
