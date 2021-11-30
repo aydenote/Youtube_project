4924f8c119c0d7bc8e05d51f69cca15c7999e386
WebDevelop2 <dslgpgh@gmail.com>
2021-12-01 00:33:37 +0900

    make search item

--- src/app.jsx
+++ src/app.jsx
@@ -4,21 +4,39 @@ import './app.css';
 
 function App() {
   const [title, setTitle] = useState([]);
-
+  
   useEffect(()=> { 
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
     
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&location=korea&key=AIzaSyA2HgFe8-5As2Uz5b1yoI1hpGGOpsOY0sk", requestOptions)
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&location=korea&key=AIzaSyATcQur2qPzkLrqDQghP40GcxGL3Piu0Eg", requestOptions)
       .then(response => response.json())
       .then(result => setTitle(result.items))
       .catch(error => console.log('error', error));
   },[]);
 
+  const search = (text)=> {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    
+    const search_text = text; 
+
+    // https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search_text}&key=AIzaSyATcQur2qPzkLrqDQghP40GcxGL3Piu0Eg
+
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search_text}}&key=AIzaSyATcQur2qPzkLrqDQghP40GcxGL3Piu0Eg`, requestOptions)
+      .then(response => response.json())
+      .then(result => setTitle(result.items))
+      .catch(error => console.log('error', error));
+  }  
+
+ return <VideoList videos={title} videoSearch={search} />
   
- return <VideoList videos={title} />;
-}
+ 
+ }
 
 export default App;
--- src/components/videoList/videoList.jsx
+++ src/components/videoList/videoList.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import VideoItem from '../videoItem/videoItem';
 import styles from '../videoList/videoList.module.css';
 
-const VideoList = (props) => (
-    
+const VideoList = (props) => {
+    const inputRef = React.createRef();
+    const formRef = React.createRef();
+
+    onsubmit = event =>{
+        event.preventDefault();
+        const text = inputRef.current.value;
+        text && props.videoSearch(text);
+    }
+    return <>
+    <div className={styles.navbar}>
+        <i class="fab fa-youtube"></i>
+          Youtube
+      <form ref={formRef} className={styles.search_form} onSubmit={onsubmit}>
+        <input ref={inputRef} type="text" className={styles.search_text} />
+      </form>
+    </div>
     <ul className={styles.videos}>
         {props.videos.map(video =>( 
             <VideoItem key={video.id} video={video} />  
         ))}
     </ul>
-            
-);
+    </>
+}
 
 export default VideoList;
